0988aae6c9c4b205d48e90d1231b2a91efad13cc
william chu <william.chu@uptickhq.com>
2023-04-05 20:24:18 +1000

    feat: added multiple mypy overrides

--- tests/test_maho.py
+++ tests/test_maho.py
@@ -1,9 +1,11 @@
-from typing import Optional
+import re
+from typing import Optional, Set
 
 import pytest
 
-from mypy_silent.maho import add_type_ignore_comment
-from mypy_silent.maho import remove_type_ignore_comment
+from mypy_silent.maho import add_type_ignore_comment, remove_type_ignore_comment
+
+_type_ignore_re = re.compile(r"# type: ignore(\[(?P<error_code>[a-z, \-]+)\])?")
 
 
 @pytest.mark.parametrize(
@@ -51,3 +53,29 @@ def test_add_type_ignore_comment(
 )
 def test_remove_type_ignore_comment(input: str, output: str) -> None:
     assert remove_type_ignore_comment(input) == output
+
+
+def test_add_type_ignore_with_existing_comment() -> None:
+    input = "host, port, protocol = m.groups()  # type: ignore\r\n"
+
+    assert (
+        add_type_ignore_comment(input, error_code=None)
+        == "host, port, protocol = m.groups()  # type: ignore\r\n"
+    )
+    assert (
+        add_type_ignore_comment(input, error_code="arg-type")
+        == "host, port, protocol = m.groups()  # type: ignore[arg-type]\r\n"
+    )
+
+
+def test_add_type_ignore_with_existing_comment_with_code() -> None:
+    input = "host, port, protocol = m.groups()  # type: ignore[misc]\r\n"
+
+    assert (
+        add_type_ignore_comment(input, error_code="arg-type")
+        == "host, port, protocol = m.groups()  # type: ignore[arg-type, misc]\r\n"
+    )
+    assert (
+        add_type_ignore_comment(input, error_code=None)
+        == "host, port, protocol = m.groups()  # type: ignore[misc]\r\n"
+    )
