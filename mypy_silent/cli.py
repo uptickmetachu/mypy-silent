9fa5a8234af1c239ce577b1db36e468ad563c11b
william chu <william.chu@uptickhq.com>
2023-04-05 20:29:44 +1000

    feat: add --nofmt option to skip black formatting

--- mypy_silent/cli.py
+++ mypy_silent/cli.py
@@ -1,19 +1,22 @@
-from typing import Optional
-from typing import Set
+from typing import Optional, Set
 
 import typer
 
-from mypy_silent.maho import add_type_ignore_comment
-from mypy_silent.maho import remove_type_ignore_comment
-from mypy_silent.parser import FilePosition
-from mypy_silent.parser import get_info_from_mypy_output
-from mypy_silent.parser import UNUSED_IGNORE_MESSAGES
+from mypy_silent.maho import add_type_ignore_comment, remove_type_ignore_comment
+from mypy_silent.parser import (
+    UNUSED_IGNORE_MESSAGES,
+    FilePosition,
+    get_info_from_mypy_output,
+)
 from mypy_silent.utils import get_lines
 
 
 def mypy_silent(
     mypy_output_file: Optional[str] = typer.Argument(
         None, help="Read mypy output from given file. Defaults to read from stdin"
+    ),
+    nofmt: bool = typer.Option(
+        False, help="append a fmt: skip to each line to skip formatting"
     ),
 ) -> None:
     lines = get_lines(mypy_output_file)
@@ -30,7 +33,7 @@ def mypy_silent(
             new_content = remove_type_ignore_comment(old_content)
         else:
             new_content = add_type_ignore_comment(
-                old_content, error_code=info.error_code
+                old_content, error_code=info.error_code, nofmt=nofmt
             )
         file_contents[info.position.line - 1] = new_content
         with open(info.position.filename, "w") as f:
--- mypy_silent/maho.py
+++ mypy_silent/maho.py
@@ -4,7 +4,9 @@ from typing import Optional
 _type_ignore_re = re.compile(r"# type: ignore(\[(?P<error_code>[a-z, \-]+)\])?")
 
 
-def add_type_ignore_comment(line: str, error_code: Optional[str]) -> str:
+def add_type_ignore_comment(
+    line: str, error_code: Optional[str], nofmt: bool = False
+) -> str:
     content_without_crlf = line.rstrip("\r\n")
     line_ending = line[len(content_without_crlf) :]
     error_codes: set[str] = set()
@@ -24,13 +26,14 @@ def add_type_ignore_comment(line: str, error_code: Optional[str]) -> str:
 
     if error_codes:
         type_ignore_comment += f"[{', '.join(sorted(error_codes))}]"
+        if nofmt:
+            type_ignore_comment += " # fmt: skip"
 
     # Workarounds for https://mypy.readthedocs.io/en/stable/common_issues.html#silencing-linters
     if "# noqa" in content_without_crlf:
         return content_without_crlf.replace(
             "# noqa", f"{type_ignore_comment} # noqa", 1
         )
-
     return content_without_crlf.rstrip() + f"  {type_ignore_comment}" + line_ending
 
 
